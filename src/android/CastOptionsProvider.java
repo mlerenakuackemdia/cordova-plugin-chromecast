5677c1d3127c361ed7efeea75f154b59dc7f53aa
mlerena <manuellerena@gmail.com>
2024-04-29 11:28:56 -0300

    Change cast controls.

--- src/android/CastOptionsProvider.java
+++ src/android/CastOptionsProvider.java
@@ -1,12 +1,17 @@
 package acidhax.cordova.chromecast;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.google.android.gms.cast.framework.CastOptions;
 import com.google.android.gms.cast.framework.OptionsProvider;
 import com.google.android.gms.cast.framework.SessionProvider;
+import com.google.android.gms.cast.framework.media.CastMediaOptions;
+import com.google.android.gms.cast.framework.media.MediaIntentReceiver;
+import com.google.android.gms.cast.framework.media.NotificationOptions;
 
 import android.content.Context;
+import android.text.format.DateUtils;
 
 public final class CastOptionsProvider implements OptionsProvider {
 
@@ -23,8 +28,28 @@ public final class CastOptionsProvider implements OptionsProvider {
 
     @Override
     public CastOptions getCastOptions(Context context) {
+        // Example showing 4 buttons: "rewind", "play/pause", "forward" and "stop casting".
+        List<String> buttonActions = new ArrayList<>();
+        buttonActions.add(MediaIntentReceiver.ACTION_REWIND);
+        buttonActions.add(MediaIntentReceiver.ACTION_TOGGLE_PLAYBACK);
+        buttonActions.add(MediaIntentReceiver.ACTION_FORWARD);
+        buttonActions.add(MediaIntentReceiver.ACTION_STOP_CASTING);
+
+// Showing "play/pause" and "stop casting" in the compat view of the notification.
+        int[] compatButtonActionsIndices = new int[]{1, 3};
+
+        // Builds a notification with the above actions. Each tap on the "rewind" and "forward" buttons skips 30 seconds.
+// Tapping on the notification opens an Activity with class VideoBrowserActivity.
+        NotificationOptions notificationOptions = new NotificationOptions.Builder()
+                .setActions(buttonActions, compatButtonActionsIndices)
+                .build();
+
+        CastMediaOptions mediaOptions = new CastMediaOptions.Builder()
+                .setNotificationOptions(notificationOptions)
+                .build();
         return new CastOptions.Builder()
                 .setReceiverApplicationId(appId)
+                .setCastMediaOptions(mediaOptions)
                 .build();
     }
     @Override
